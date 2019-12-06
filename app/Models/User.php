538d3a8394350a135f7ea030312d645fe1666f2a
vagrant <vagrant@homestead>
2019-12-06 07:38:45 +0000

    首页微博列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,11 @@ class User extends Authenticatable
     {
         return $this->hasMany(Status::class);
     }
+    public function feed()
+    {
+        return $this->statuses()
+            ->orderBy('created_at', 'desc');
+    }
 
     /**
      * The attributes that should be hidden for arrays.
