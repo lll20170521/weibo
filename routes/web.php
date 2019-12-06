5dffcce5ca12107e91733526eda3e4850663da4a
vagrant <vagrant@homestead>
2019-12-06 03:45:42 +0000

    充值密码

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,29 @@
 */
 
 Route::get('/', 'StaticPagesController@home')->name('home');
+
 Route::get('/help', 'StaticPagesController@help')->name('help');
+
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
+
 Route::resource('users', 'UsersController');
+
 Route::get('login', 'SessionsController@create')->name('login');
+
 Route::post('login', 'SessionsController@store')->name('login');
+
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
 Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
